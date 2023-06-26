a2d7c87e9b8f64a1ba4aee4252dc387806f86a8f
Justus Magin <keewis@posteo.de>
2023-06-26 17:38:36 +0200

    add tests for the product id decoder

--- alos2/tests/test_decoders.py
+++ alos2/tests/test_decoders.py
@@ -40,3 +40,66 @@ def test_decode_scene_id(scene_id, expected):
     actual = decoders.decode_scene_id(scene_id)
 
     assert actual == expected
+
+
+@pytest.mark.parametrize(
+    ["product_id", "expected"],
+    (
+        pytest.param(
+            "WWDR1.1__D",
+            {
+                "observation_mode": "ScanSAR nominal 28MHz mode dual polarization",
+                "observation_direction": "right looking",
+                "processing_level": "level 1.1",
+                "processing_option": "not specified",
+                "map_projection": "not specified",
+                "orbit_direction": "descending",
+            },
+            id="valid_id-l11rd",
+        ),
+        pytest.param(
+            "WWDL1.1__A",
+            {
+                "observation_mode": "ScanSAR nominal 28MHz mode dual polarization",
+                "observation_direction": "left looking",
+                "processing_level": "level 1.1",
+                "processing_option": "not specified",
+                "map_projection": "not specified",
+                "orbit_direction": "ascending",
+            },
+            id="valid_id-l11la",
+        ),
+        pytest.param(
+            "WWDR1.5RUA",
+            {
+                "observation_mode": "ScanSAR nominal 28MHz mode dual polarization",
+                "observation_direction": "right looking",
+                "processing_level": "level 1.5",
+                "processing_option": "geo-reference",
+                "map_projection": "UTM",
+                "orbit_direction": "ascending",
+            },
+            id="valid_id-l15rd",
+        ),
+        pytest.param(
+            "WWDR1.6__A",
+            ValueError("invalid product id"),
+            id="invalid_id-invalid_level",
+        ),
+        pytest.param(
+            "WRDR1.1__A",
+            ValueError("invalid product id"),
+            id="invalid_id-wrong_observation_mode",
+        ),
+    ),
+)
+def test_decode_product_id(product_id, expected):
+    if issubclass(type(expected), Exception):
+        with pytest.raises(type(expected), match=expected.args[0]):
+            decoders.decode_product_id(product_id)
+
+        return
+
+    actual = decoders.decode_product_id(product_id)
+
+    assert actual == expected
