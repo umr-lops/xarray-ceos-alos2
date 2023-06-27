c4dde37ad896b72ec9f744171173287ecc9d4296
Justus Magin <keewis@users.noreply.github.com>
2023-06-27 16:46:58 +0200

    define the structure of the volume directory file (#5)
    * define adapters for the special data types of CEOS
    
    * add tests for the new datatypes
    
    * define the CEOS record preamble
    
    * define the structure of the volume directory file
    
    * add `construct` to the dependencies

---
+++ ceos_alos2/tests/test_datatypes.py
@@ -0,0 +1,50 @@
+import pytest
+
+from ceos_alos2 import datatypes
+
+
+@pytest.mark.parametrize(
+    ["data", "n_bytes", "expected"],
+    (
+        pytest.param(b"15", 2, 15, id="2bytes-no_padding"),
+        pytest.param(b"3989", 4, 3989, id="4bytes-no_padding"),
+        pytest.param(b"16  ", 4, 16, id="4bytes-padding"),
+    ),
+)
+def test_ascii_integer(data, n_bytes, expected):
+    parser = datatypes.AsciiInteger(n_bytes)
+
+    actual = parser.parse(data)
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    ["data", "n_bytes", "expected"],
+    (
+        pytest.param(b"1558.423", 8, 1558.423, id="8bytes-no_padding"),
+        pytest.param(b" 165.820", 8, 165.820, id="8bytes-padding"),
+        pytest.param(
+            b"162436598487.832", 16, 162436598487.832, id="16bytes-no_padding"
+        ),
+        pytest.param(b"     6598487.832", 16, 6598487.832, id="16bytes-padding"),
+    ),
+)
+def test_ascii_float(data, n_bytes, expected):
+    parser = datatypes.AsciiFloat(n_bytes)
+
+    actual = parser.parse(data)
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    ["data", "n_bytes", "expected"],
+    (
+        pytest.param(b"ALOS", 4, "ALOS", id="4bytes-no_padding"),
+        pytest.param(b"abc ", 4, "abc", id="4bytes-padding"),
+    ),
+)
+def test_padded_string(data, n_bytes, expected):
+    parser = datatypes.PaddedString(n_bytes)
+
+    actual = parser.parse(data)
+    assert actual == expected
