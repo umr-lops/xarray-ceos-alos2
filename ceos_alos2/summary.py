e4dc7f34fa78521c7a51b74a6f1e2cfc77accbe1
Justus Magin <keewis@users.noreply.github.com>
2023-06-27 16:23:36 +0200

    read the summary (#4)
    * import `ExceptionGroup` for compatibility with python 3.10
    
    * move the local functions to module scope
    
    * move the remaining files to `ceos_alos2`
    
    * rename the module import in the decoder tests
    
    * add tests for the summary parser
    
    * refactor the summary reader and add tests

---
+++ ceos_alos2/summary.py
@@ -0,0 +1,54 @@
+import re
+
+from tlz.dicttoolz import merge, valmap
+from tlz.functoolz import compose_left, curry
+from tlz.itertoolz import groupby
+
+try:
+    ExceptionGroup
+except NameError:
+    from exceptiongroup import ExceptionGroup
+
+entry_re = re.compile(r'(?P<section>[A-Za-z]{3})_(?P<keyword>.*?)="(?P<value>.*?)"')
+
+
+def parse_line(line):
+    match = entry_re.fullmatch(line)
+    if match is None:
+        raise ValueError("invalid line")
+
+    return match.groupdict()
+
+
+def with_lineno(e, lineno):
+    message = e.args[0]
+
+    e.args = (f"line {lineno:02d}: {message}",) + e.args[1:]
+
+    return e
+
+
+def parse_summary(content):
+    lines = content.splitlines()
+
+    entries = []
+    errors = {}
+    for lineno, line in enumerate(lines):
+        try:
+            parsed = parse_line(line)
+            entries.append(parsed)
+        except ValueError as e:
+            errors[lineno] = e
+
+    if errors:
+        new_errors = [with_lineno(error, lineno) for lineno, error in errors.items()]
+        raise ExceptionGroup("failed to parse the summary", new_errors)
+
+    merge_sections = compose_left(
+        curry(groupby, lambda x: x["section"]),
+        curry(
+            valmap,
+            compose_left(curry(map, lambda x: {x["keyword"]: x["value"]}), merge),
+        ),
+    )
+    return merge_sections(entries)
---
+++ ceos_alos2/tests/test_summary.py
@@ -0,0 +1,82 @@
+import pytest
+
+from ceos_alos2 import summary
+
+try:
+    ExceptionGroup
+except NameError:
+    from exceptiongroup import ExceptionGroup
+
+
+def compare_exceptions(e1, e2):
+    return type(e1) == type(e2) and e1.args == e2.args
+
+
+@pytest.mark.parametrize(
+    ["line", "expected"],
+    (
+        pytest.param(
+            'Scs_SceneShift="0"',
+            {"section": "Scs", "keyword": "SceneShift", "value": "0"},
+            id="valid_line1",
+        ),
+        pytest.param(
+            'Pds_ProductID="WWDR1.1__D"',
+            {"section": "Pds", "keyword": "ProductID", "value": "WWDR1.1__D"},
+            id="valid_line2",
+        ),
+        pytest.param(
+            'Scs_SceneShift"0"', ValueError("invalid line"), id="invalid_line1"
+        ),
+        pytest.param(
+            'PdsProductID="WWDR1.1__D"',
+            ValueError("invalid line"),
+            id="invalid_line2",
+        ),
+    ),
+)
+def test_parse_line(line, expected):
+    if isinstance(expected, Exception):
+        with pytest.raises(type(expected), match=expected.args[0]):
+            summary.parse_line(line)
+        return
+
+    actual = summary.parse_line(line)
+
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    ["content", "expected"],
+    (
+        pytest.param(
+            'Scs_SceneShift="0"\nPds_ProductID="WWDR1.1__D"',
+            {"Scs": {"SceneShift": "0"}, "Pds": {"ProductID": "WWDR1.1__D"}},
+            id="valid_lines",
+        ),
+        pytest.param(
+            'Scs_SceneShift"0"\nPdsProductID="WWDR1.1__D"',
+            ExceptionGroup(
+                "failed to parse the summary",
+                [
+                    ValueError("line 00: invalid line"),
+                    ValueError("line 01: invalid line"),
+                ],
+            ),
+            id="invalid_lines",
+        ),
+    ),
+)
+def test_parse_summary(content, expected):
+    if isinstance(expected, Exception):
+        with pytest.raises(type(expected)) as e:
+            summary.parse_summary(content)
+
+        assert e.value.message == expected.message
+        assert all(
+            compare_exceptions(e1, e2)
+            for e1, e2 in zip(e.value.exceptions, expected.exceptions)
+        )
+        return
+    actual = summary.parse_summary(content)
+    assert actual == expected
