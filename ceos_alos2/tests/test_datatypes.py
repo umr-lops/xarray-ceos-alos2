bffec287bdf038ecc49a5a27e40cbb137bb1f993
Justus Magin <keewis@posteo.de>
2023-06-28 18:07:24 +0200

    improve the test coverage of `AsciiFloat`

--- ceos_alos2/tests/test_datatypes.py
+++ ceos_alos2/tests/test_datatypes.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 
 from ceos_alos2 import datatypes
@@ -24,6 +25,7 @@ def test_ascii_integer(data, n_bytes, expected):
     (
         pytest.param(b"1558.423", 8, 1558.423, id="8bytes-no_padding"),
         pytest.param(b" 165.820", 8, 165.820, id="8bytes-padding"),
+        pytest.param(b"        ", 8, float("nan"), id="8bytes-all_padding"),
         pytest.param(b"162436598487.832", 16, 162436598487.832, id="16bytes-no_padding"),
         pytest.param(b"     6598487.832", 16, 6598487.832, id="16bytes-padding"),
     ),
@@ -32,7 +34,7 @@ def test_ascii_float(data, n_bytes, expected):
     parser = datatypes.AsciiFloat(n_bytes)
 
     actual = parser.parse(data)
-    assert actual == expected
+    np.testing.assert_equal(actual, expected)
 
 
 @pytest.mark.parametrize(
