3ffa845019f13b9695d9f9683b580cedb5eec7e5
Justus Magin <keewis@posteo.de>
2023-07-06 14:07:45 +0200

    add tests for `keysplit` and `valsplit`

--- ceos_alos2/tests/test_dicttoolz.py
+++ ceos_alos2/tests/test_dicttoolz.py
@@ -21,3 +21,19 @@ def test_itemsplit(data, expected):
 
     actual = dicttoolz.itemsplit(predicate, data)
     assert actual == expected
+
+
+def test_valsplit():
+    data = {0: 1, 1: 0, 2: 2}
+    actual = dicttoolz.valsplit(lambda v: v != 0, data)
+    expected = ({0: 1, 2: 2}, {1: 0})
+
+    assert actual == expected
+
+
+def test_keysplit():
+    data = {0: 1, 1: 0, 2: 2}
+    actual = dicttoolz.keysplit(lambda k: k % 2 == 0, data)
+    expected = ({0: 1, 2: 2}, {1: 0})
+
+    assert actual == expected
