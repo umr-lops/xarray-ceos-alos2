6c5a74f605fb7c25a57d9ab0f8b492c2fb93cd7f
Justus Magin <keewis@posteo.de>
2023-07-25 16:47:33 +0200

    replace the file object with a simple `open` function

--- ceos_alos2/file.py
+++ ceos_alos2/file.py
@@ -1,7 +1,6 @@
 import fsspec
 from fsspec.implementations.dirfs import DirFileSystem
 from rich.console import Console
-from tlz.dicttoolz import get_in
 from tlz.functoolz import curry
 
 from ceos_alos2 import sar_image
@@ -18,108 +17,114 @@ from ceos_alos2.summary import parse_summary
 console = Console()
 
 
-class File:
-    def read_image(self, fs, path):
-        with fs.open(path, mode="rb") as f:
-            header, metadata = sar_image.read_metadata(f, self.chunks[0])
+def read_summary(mapper, path):
+    try:
+        bytes_ = mapper[path]
+    except FileNotFoundError as e:
+        raise OSError(
+            f"Cannot find the summary file (`{path}`)."
+            f" Make sure the dataset at {mapper.root} is complete and in the JAXA CEOS format."
+        ) from e
 
-        byte_ranges = [(m.data.start, m.data.stop) for m in metadata]
-        type_code = sar_image.extract_format_type(header)
-        parser = curry(sar_image.parse_data, type_code=type_code)
+    return parse_summary(bytes_.decode())
 
-        shape = sar_image.extract_shape(header)
-        dtype = sar_image.extract_dtype(header)
 
-        image_data = Array(
-            fs=fs,
-            url=path,
-            byte_ranges=byte_ranges,
-            shape=shape,
-            dtype=dtype,
-            parse_bytes=parser,
-            chunks=self.chunks,
-        )
+def read_image(fs, path, chunks):
+    with fs.open(path, mode="rb") as f:
+        header, metadata = sar_image.read_metadata(f, chunks[0])
 
-        # transform metadata:
-        # - group attrs
-        # - coords
-        # - image variable attrs
-        group_attrs = sar_image.extract_attrs(header)
-        coords, var_attrs = sar_image.transform_metadata(metadata)
-        image_variable = (["rows", "columns"], image_data, var_attrs)
+    byte_ranges = [(m.data.start, m.data.stop) for m in metadata]
+    type_code = sar_image.extract_format_type(header)
+    parser = curry(sar_image.parse_data, type_code=type_code)
 
-        raw_variables = coords | {"data": image_variable}
-        variables = {name: Variable(*var) for name, var in raw_variables.items()}
+    shape = sar_image.extract_shape(header)
+    dtype = sar_image.extract_dtype(header)
 
-        return Group(path=None, data=variables, attrs=group_attrs)
+    image_data = Array(
+        fs=fs,
+        url=path,
+        byte_ranges=byte_ranges,
+        shape=shape,
+        dtype=dtype,
+        parse_bytes=parser,
+        chunks=chunks,
+    )
 
-    def __init__(self, url_or_dirfs, chunks=None, **options):
-        if isinstance(url_or_dirfs, DirFileSystem):
-            self.fs = url_or_dirfs
-        elif isinstance(url_or_dirfs, str):
-            fs, url = fsspec.core.url_to_fs(url_or_dirfs)
-            self.fs = DirFileSystem(path=url, fs=fs)
-        else:
-            raise ValueError(f"unkown type: {url_or_dirfs}")
+    # transform metadata:
+    # - group attrs
+    # - coords
+    # - image variable attrs
+    group_attrs = sar_image.extract_attrs(header)
+    coords, var_attrs = sar_image.transform_metadata(metadata)
+    image_variable = (["rows", "columns"], image_data, var_attrs)
 
-        if chunks is None:
-            chunks = (1024, -1)
-        self.chunks = chunks
+    raw_variables = coords | {"data": image_variable}
+    variables = {name: Variable(*var) for name, var in raw_variables.items()}
 
-        try:
-            with self.fs.open("summary.txt", mode="r") as f:
-                summary = parse_summary(f.read())
-        except FileNotFoundError as e:
-            raise OSError(
-                "Cannot find the summary file (`summary.txt`)."
-                f" Make sure the dataset at {url} is complete and in the JAXA CEOS format."
-            ) from e
+    group_name = sar_image.filename_to_groupname(path)
 
-        fnames = summary["Pdi"]["data_files"]
+    return Group(path=group_name, data=variables, attrs=group_attrs)
 
-        # try:
-        #     with self.fs.open(fnames["volume_directory"], mode="rb") as f:
-        #         parsed = volume_directory.volume_directory_record.parse(f.read())
-        #         vol_attrs = volume_directory.extract_metadata(parsed)
-        # except FileNotFoundError as e:
-        #     raise OSError(
-        #         f"Cannot find the volume directory file (`{fnames['volume_directory']}`)."
-        #         f" Make sure the dataset at {url} is complete and in the JAXA CEOS format."
-        #     )
 
-        # try:
-        #     with self.fs.open(fnames["sar_leader"], mode="rb") as f:
-        #         sar_leader = sar_leader_record.parse(f.read())
-        # except FileNotFoundError as e:
-        #     raise OSError(
-        #         f"Cannot find the sar leader file (`{fnames['sar_leader']}`)."
-        #         f" Make sure the dataset at {url} is complete and in the JAXA CEOS format."
-        #     )
+def open(path, chunks=None, storage_options={}):
+    mapper = fsspec.get_mapper(path, **storage_options)
+    dirfs = DirFileSystem(fs=mapper.fs, path=mapper.root)
 
-        image_groups = {
-            sar_image.filename_to_groupname(path): self.read_image(self.fs, path)
-            for path in fnames["sar_imagery"]
-        }
+    # fill in fallback chunks
+    if chunks is None:
+        chunks = (1024, -1)
 
-        self._groups = image_groups
+    # read summary
+    # TODO: split into metadata for the reader and human-readable metadata
+    summary = read_summary(mapper, "summary.txt")
 
-        # try:
-        #     with dirfs.open(fnames["sar_trailer"], mode="rb") as f:
-        #         trailer_header, image_ranges = read_sar_trailer_metadata(f)
-        # except FileNotFoundError as e:
-        #     raise OSError(
-        #         f"Cannot find the sar trailer file (`{fnames['sar_trailer']}`)."
-        #         f" Make sure the dataset at {url} is complete and in the JAXA CEOS format."
-        #     )
+    filenames = summary["Pdi"]["data_files"]
 
-    def __getitem__(self, path):
-        parts = path.lstrip("/").split("/")
-        return get_in(parts, self._groups, no_default=True)
+    # read volume directory
+    # read sar leader
+    # read actual imagery
+    imagery_groups = [
+        read_image(dirfs, path, chunks, **storage_options) for path in filenames["sar_imagery"]
+    ]
+    imagery = Group("/imagery", data={group.name: group for group in imagery_groups}, attrs={})
+    # read sar trailer
 
-    @property
-    def groups(self):
-        return self._groups
+    subgroups = {"imagery": imagery}
 
-    @property
-    def filename(self):
-        return self.fs.path
+    return Group(path="/", data=subgroups, attrs={})
+
+
+# try:
+#     with self.fs.open(fnames["volume_directory"], mode="rb") as f:
+#         parsed = volume_directory.volume_directory_record.parse(f.read())
+#         vol_attrs = volume_directory.extract_metadata(parsed)
+# except FileNotFoundError as e:
+#     raise OSError(
+#         f"Cannot find the volume directory file (`{fnames['volume_directory']}`)."
+#         f" Make sure the dataset at {url} is complete and in the JAXA CEOS format."
+#     )
+
+# try:
+#     with self.fs.open(fnames["sar_leader"], mode="rb") as f:
+#         sar_leader = sar_leader_record.parse(f.read())
+# except FileNotFoundError as e:
+#     raise OSError(
+#         f"Cannot find the sar leader file (`{fnames['sar_leader']}`)."
+#         f" Make sure the dataset at {url} is complete and in the JAXA CEOS format."
+#     )
+
+# image_groups = {
+#     sar_image.filename_to_groupname(path): self.read_image(self.fs, path)
+#     for path in fnames["sar_imagery"]
+# }
+
+# self._groups = image_groups
+
+# try:
+#     with dirfs.open(fnames["sar_trailer"], mode="rb") as f:
+#         trailer_header, image_ranges = read_sar_trailer_metadata(f)
+# except FileNotFoundError as e:
+#     raise OSError(
+#         f"Cannot find the sar trailer file (`{fnames['sar_trailer']}`)."
+#         f" Make sure the dataset at {url} is complete and in the JAXA CEOS format."
+#     )
