742b7c9e9a8d57c0608fe04c34a780484ef4d17c
Justus Magin <keewis@posteo.de>
2023-08-07 17:32:01 +0200

    test the `construct` to standard-python converter util

--- ceos_alos2/tests/test_utils.py
+++ ceos_alos2/tests/test_utils.py
@@ -1,4 +1,8 @@
+import datetime
+
 import pytest
+from construct import EnumIntegerString
+from construct.lib.containers import ListContainer
 
 from ceos_alos2 import utils
 
@@ -12,5 +16,47 @@ from ceos_alos2 import utils
 )
 def test_unique(data, expected):
     actual = utils.unique(data)
+
+    assert actual == expected
+
+
+def test_to_dict():
+    container = {
+        "_io": None,
+        "a": {
+            "aa": EnumIntegerString.new(1, "value"),
+            "ab": 1,
+            "ac": "ac",
+            "ad": b"ad",
+            "ae": 1j,
+            "af": datetime.datetime(1999, 1, 1, 0, 0, 0),
+        },
+        "b": ListContainer(
+            [
+                {"ba1": 1},
+                {"ba2": 1},
+            ]
+        ),
+        "c": (1, 2, 3),
+        "d": [1, 2, 3],
+    }
+    expected = {
+        "a": {
+            "aa": "value",
+            "ab": 1,
+            "ac": "ac",
+            "ad": b"ad",
+            "ae": 1j,
+            "af": datetime.datetime(1999, 1, 1, 0, 0, 0),
+        },
+        "b": [
+            {"ba1": 1},
+            {"ba2": 1},
+        ],
+        "c": (1, 2, 3),
+        "d": [1, 2, 3],
+    }
+
+    actual = utils.to_dict(container)
 
     assert actual == expected
