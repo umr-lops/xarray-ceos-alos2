339b29e7e21c3ab5fdbf8cbef3c6416fbda384d4
Justus Magin <keewis@posteo.de>
2023-08-10 11:49:16 +0200

    add tests for the volume descriptor metadata transformer

--- ceos_alos2/tests/test_volume_directory.py
+++ ceos_alos2/tests/test_volume_directory.py
@@ -15,3 +15,69 @@ class TestMetadata:
         actual = metadata.normalize_datetime(s)
 
         assert actual == expected
+
+    @pytest.mark.parametrize(
+        ["mapping", "expected"],
+        (
+            pytest.param(
+                {
+                    "preamble": {"a": 1},
+                    "ascii_ebcdic_flag": "A",
+                    "blanks": "",
+                    "spare": "",
+                    "local_use_segment": "",
+                    "total_number_of_physical_volumes_in_logical_volume": 1,
+                    "physical_volume_sequence_number_of_the_first_tape": 1,
+                    "physical_volume_sequence_number_of_the_last_tape": 1,
+                    "physical_volume_sequence_number_of_the_current_tape": 1,
+                    "file_number_in_the_logical_volume": 2,
+                    "logical_volume_within_a_volume_set": "a",
+                    "logical_volume_number_within_physical_volume": 4,
+                    "number_of_file_pointer_records": 4,
+                    "number_of_text_records_in_volume_directory": 1,
+                },
+                {},
+                id="ignored1",
+            ),
+            pytest.param(
+                {"number_of_file_pointer_records": 4, "volume_set_id": "abc"},
+                {"volume_set_id": "abc"},
+                id="ignored2",
+            ),
+            pytest.param(
+                {
+                    "superstructure_format_control_document_id": "a",
+                    "superstructure_format_control_document_revision_level": "a",
+                    "superstructure_record_format_revision_level": "a",
+                    "software_release_and_revision_level": "001.001",
+                    "logical_volume_generation_country": "a",
+                    "logical_volume_generating_agency": "a",
+                    "logical_volume_generating_facility": "a",
+                },
+                {
+                    "control_document_id": "a",
+                    "control_document_revision_level": "a",
+                    "record_format_revision_level": "a",
+                    "software_version": "001.001",
+                    "creation_country": "a",
+                    "creation_agency": "a",
+                    "creation_facility": "a",
+                },
+                id="translations1",
+            ),
+            pytest.param(
+                {"logical_volume_creation_datetime": "2020101117233798"},
+                {"creation_datetime": "2020-10-11T17:23:37.980000"},
+                id="translations2",
+            ),
+            pytest.param(
+                {"creation_datetime": "2020101117233798"},
+                {"creation_datetime": "2020-10-11T17:23:37.980000"},
+                id="postprocessing",
+            ),
+        ),
+    )
+    def test_transform_volume_descriptor(self, mapping, expected):
+        actual = metadata.transform_volume_descriptor(mapping)
+
+        assert actual == expected
