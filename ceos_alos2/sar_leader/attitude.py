b4ea83c8a1f51b58104208c9ff304f9b27fafac1
Justus Magin <keewis@posteo.de>
2023-08-18 14:27:13 +0200

    transform the attitude data

--- ceos_alos2/sar_leader/attitude.py
+++ ceos_alos2/sar_leader/attitude.py
@@ -1,7 +1,13 @@
+import numpy as np
 from construct import Struct, this
+from tlz.dicttoolz import assoc_in, get_in, merge_with, valmap
+from tlz.functoolz import curry, pipe
+from tlz.itertoolz import cons, get
 
 from ceos_alos2.common import record_preamble
 from ceos_alos2.datatypes import AsciiFloat, AsciiInteger, Metadata, PaddedString
+from ceos_alos2.dicttoolz import apply_to_items, dissoc
+from ceos_alos2.transformers import as_group, separate_attrs
 
 attitude_point = Struct(
     "time"
@@ -35,3 +41,80 @@ attitude_record = Struct(
     "data_points" / attitude_point[this.number_of_points],
     "blanks" / PaddedString(this.preamble.record_length - (12 + 4 + this.number_of_points * 120)),
 )
+
+
+def transform_time(mapping):
+    # no year information, so we have to convert to timedelta
+    units = {"day_of_year": "D", "millisecond_of_day": "ms"}
+    transformed = {k: np.asarray(v, dtype=f"timedelta64[{units[k]}]") for k, v in mapping.items()}
+
+    return (transformed["day_of_year"] + transformed["millisecond_of_day"]).astype(
+        "timedelta64[ns]"
+    )
+
+
+def transpose_nested(mapping):
+    def _transpose(value):
+        if not isinstance(value, list) or not value or not isinstance(value[0], dict):
+            return value
+
+        return merge_with(list, *value)
+
+    return pipe(
+        mapping,
+        curry(_transpose),
+        curry(valmap, _transpose),
+    )
+
+
+def prepend_dim(dim, var):
+    if isinstance(var, dict):
+        return valmap(curry(prepend_dim, dim), var)
+
+    if not isinstance(var, tuple):
+        var = (var, {})
+
+    return tuple(cons(dim, var))
+
+
+def copy(instructions, mapping):
+    new = mapping
+    for dest, source in instructions.items():
+        new = assoc_in(new, list(dest), get_in(source, mapping))
+
+    return new
+
+
+def transform_section(mapping):
+    transformers = {
+        "pitch": separate_attrs,
+        "roll": separate_attrs,
+        "yaw": separate_attrs,
+        "pitch_error": lambda arr: np.asarray(arr, dtype=bool),
+        "roll_error": lambda arr: np.asarray(arr, dtype=bool),
+        "yaw_error": lambda arr: np.asarray(arr, dtype=bool),
+    }
+
+    return apply_to_items(transformers, mapping)
+
+
+def transform_attitude(mapping):
+    transformers = {
+        "time": transform_time,
+        "attitude": transform_section,
+        "rates": transform_section,
+    }
+
+    result = pipe(
+        mapping,
+        curry(get, "data_points"),
+        curry(transpose_nested),
+        curry(apply_to_items, transformers),
+        curry(prepend_dim, "points"),
+        curry(copy, {("attitude", "time"): ["time"], ("rates", "time"): ["time"]}),
+        curry(dissoc, ["time"]),
+        curry(valmap, lambda x: (x, {"coordinates": ["time"]})),
+        curry(as_group),
+    )
+
+    return result
