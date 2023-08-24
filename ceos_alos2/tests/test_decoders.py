7ad2127d59cee4b0bea7c167288ad41026ed0b1b
Justus Magin <keewis@users.noreply.github.com>
2023-08-24 16:36:24 +0200

    increase the test coverage of the decoders (#46)
    * use `isinstance(obj, ...)` instead of `issubclass(type(obj), ...)`
    
    * add tests for the filename decoder

--- ceos_alos2/tests/test_decoders.py
+++ ceos_alos2/tests/test_decoders.py
@@ -36,7 +36,7 @@ from ceos_alos2 import decoders
     ),
 )
 def test_decode_scene_id(scene_id, expected):
-    if issubclass(type(expected), Exception):
+    if isinstance(expected, Exception):
         with pytest.raises(type(expected), match=expected.args[0]):
             decoders.decode_scene_id(scene_id)
 
@@ -99,7 +99,7 @@ def test_decode_scene_id(scene_id, expected):
     ),
 )
 def test_decode_product_id(product_id, expected):
-    if issubclass(type(expected), Exception):
+    if isinstance(expected, Exception):
         with pytest.raises(type(expected), match=expected.args[0]):
             decoders.decode_product_id(product_id)
 
@@ -123,12 +123,83 @@ def test_decode_product_id(product_id, expected):
     ),
 )
 def test_decode_scan_info(scan_info, expected):
-    if issubclass(type(expected), Exception):
+    if isinstance(expected, Exception):
         with pytest.raises(type(expected), match=expected.args[0]):
             decoders.decode_scan_info(scan_info)
 
         return
 
     actual = decoders.decode_scan_info(scan_info)
+
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    ["path", "expected"],
+    (
+        pytest.param(
+            "IMG-HV-ALOS2225333100-180726-WWDR1.1__D-B3",
+            {
+                "filetype": "IMG",
+                "polarization": "HV",
+                "mission_name": "ALOS2",
+                "orbit_accumulation": "22533",
+                "scene_frame": "3100",
+                "date": datetime.datetime(2018, 7, 26, 0, 0, 0),
+                "observation_mode": "ScanSAR nominal 28MHz mode dual polarization",
+                "observation_direction": "right looking",
+                "processing_level": "level 1.1",
+                "processing_option": "not specified",
+                "map_projection": "not specified",
+                "orbit_direction": "descending",
+                "processing_method": "SPECAN method",
+                "scan_number": "3",
+            },
+        ),
+        pytest.param(
+            "TRL-ALOS2225333100-180726-WWDR1.1__D",
+            {
+                "filetype": "TRL",
+                "polarization": None,
+                "mission_name": "ALOS2",
+                "orbit_accumulation": "22533",
+                "scene_frame": "3100",
+                "date": datetime.datetime(2018, 7, 26, 0, 0, 0),
+                "observation_mode": "ScanSAR nominal 28MHz mode dual polarization",
+                "observation_direction": "right looking",
+                "processing_level": "level 1.1",
+                "processing_option": "not specified",
+                "map_projection": "not specified",
+                "orbit_direction": "descending",
+            },
+        ),
+        pytest.param(
+            "LED-ALOS2290760600-191011-WWDR1.5RUA",
+            {
+                "filetype": "LED",
+                "polarization": None,
+                "mission_name": "ALOS2",
+                "orbit_accumulation": "29076",
+                "scene_frame": "0600",
+                "date": datetime.datetime(2019, 10, 11, 0, 0, 0),
+                "observation_mode": "ScanSAR nominal 28MHz mode dual polarization",
+                "observation_direction": "right looking",
+                "processing_level": "level 1.5",
+                "processing_option": "geo-reference",
+                "map_projection": "UTM",
+                "orbit_direction": "ascending",
+            },
+        ),
+        pytest.param("summary.txt", ValueError("invalid file name")),
+    ),
+)
+def test_decode_filename(path, expected):
+    if isinstance(expected, Exception):
+        with pytest.raises(type(expected), match=expected.args[0]):
+            decoders.decode_filename(path)
+
+        return
+
+    actual = decoders.decode_filename(path)
 
     assert actual == expected
