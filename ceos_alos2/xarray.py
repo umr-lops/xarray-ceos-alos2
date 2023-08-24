b6dbdb955a9e864c0489ebac07376d5c1a632bc3
Justus Magin <keewis@posteo.de>
2023-08-24 17:49:10 +0200

    refactor `to_datatree` to use `Group.subtree` and `DataTree.from_dict`

--- ceos_alos2/xarray.py
+++ ceos_alos2/xarray.py
@@ -81,10 +81,10 @@ def to_dataset(group, chunks=None):
 
 
 def to_datatree(group, chunks=None):
-    root = datatree.DataTree(data=to_dataset(group, chunks=chunks))
-    for name, subgroup in group.groups.items():
-        root[name] = to_datatree(subgroup, chunks=chunks)
-    return root
+    mapping = {"/": to_dataset(group, chunks=chunks)} | {
+        path: to_dataset(subgroup, chunks=chunks) for path, subgroup in group.subtree
+    }
+    return datatree.DataTree.from_dict(mapping)
 
 
 def open_alos2(path, chunks=None, storage_options={}, backend_options={}):
