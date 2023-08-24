5e4ee75e5f2bb2b137efbf3d990270e12fd94e65
Justus Magin <keewis@posteo.de>
2023-08-24 17:29:02 +0200

    add crude tests for the lazily indexed wrapper

--- ceos_alos2/tests/test_xarray.py
+++ ceos_alos2/tests/test_xarray.py
@@ -1,10 +1,43 @@
 import numpy as np
 import pytest
 import xarray as xr
+from xarray.core.indexing import BasicIndexer, VectorizedIndexer
 
 from ceos_alos2 import xarray
 from ceos_alos2.hierarchy import Variable
 from ceos_alos2.tests.utils import create_dummy_array
+
+
+class TestLazilyIndexedWrapper:
+    def test_init(self):
+        lock = xarray.SerializableLock()
+
+        data = create_dummy_array()
+
+        wrapper = xarray.LazilyIndexedWrapper(data, lock)
+
+        assert wrapper.shape == data.shape
+        assert wrapper.dtype == data.dtype
+        assert wrapper.array is data
+        assert wrapper.lock is lock
+
+    @pytest.mark.parametrize(
+        "indexer",
+        (
+            pytest.param(BasicIndexer((0, 1))),
+            pytest.param(VectorizedIndexer((slice(None), slice(None)))),
+        ),
+    )
+    def test_getitem(self, indexer):
+        array = np.arange(6).reshape(2, 3)
+        expected = array[indexer.tuple]
+
+        lock = xarray.SerializableLock()
+        wrapped = xarray.LazilyIndexedWrapper(array, lock)
+
+        actual = wrapped[indexer]
+
+        np.testing.assert_equal(actual, expected)
 
 
 @pytest.mark.parametrize(
