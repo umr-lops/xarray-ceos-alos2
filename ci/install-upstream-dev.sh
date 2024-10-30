a4ce9ae0f2151f8416fdd08ba87e6170d25b9187
Justus Magin <keewis@posteo.de>
2024-10-30 11:12:46 +0100

    don't install `datatree` in the dev environment, either

--- ci/install-upstream-dev.sh
+++ ci/install-upstream-dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-conda remove -y --force cytoolz numpy xarray construct toolz xarray-datatree fsspec python-dateutil pandas
+conda remove -y --force cytoolz numpy xarray construct toolz fsspec python-dateutil pandas
 python -m pip install \
   -i https://pypi.anaconda.org/scientific-python-nightly-wheels/simple \
   --no-deps \
@@ -12,6 +12,5 @@ python -m pip install \
 python -m pip install --upgrade \
   git+https://github.com/construct/construct \
   git+https://github.com/pytoolz/toolz \
-  git+https://github.com/xarray-contrib/datatree \
   git+https://github.com/fsspec/filesystem_spec \
   git+https://github.com/dateutil/dateutil
